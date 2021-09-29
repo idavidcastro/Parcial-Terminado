65041623048d3fb225bdde6d2e1445a2b87f3a80
almendralesjorge <almendralesjorge19@gmail.com>
2021-09-29 09:01:50 -0500

    metodo modificar

--- Presentación/Program.cs
+++ Presentación/Program.cs
@@ -50,5 +50,57 @@ namespace Presentation
 
 
         }
+        private static void Consultar()
+        {
+            Console.Clear();
+            var respuesta = CreditoService.Consultar();
+            if (respuesta.Error)
+            {
+                Console.WriteLine(respuesta.Mensaje);
+            }
+            else
+            {
+                foreach (var item in respuesta.Personas)
+                {
+                    Console.WriteLine(item.ToString());
+                    Console.WriteLine("-------------------------");
+                }
+            }
+
+        }
+        private static void ConsultaNumeroCredito()
+        {
+            Console.Clear();
+            Console.WriteLine("ingrese el numero del credito a modificar datos.");
+            BusquedaNumero();
+        }
+
+        private static void BusquedaNumero()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Modificar()
+        { 
+            Console.Clear();
+            Console.WriteLine("Información de la Persona a Modificar");
+            int(periodo, tipoTasaInteres, identificacionCliente) = BusquedaNumero();
+            decimal(montoDinero, valorInteres)= BusquedaNumero();
+            if (IsFind)
+            {
+                Console.WriteLine("ingrese los Nuevos Datos ");
+                Persona persona = Guardar();
+                Console.Write("Ingrese la identificacion del cliente: ");
+                string mensaje = personaService.Modificiar(identificacionCliente, persona);
+                Console.WriteLine(mensaje);
+                Console.Write("Ingrese el monto de dinero a solicitar: ");
+                string mensaje = personaService.Modificiar(montoDinero, persona);
+                Console.WriteLine(mensaje);
+                Console.Write("Ingrese el tipo de tasa de interes (1-compuesto ; 2-simple): ");
+                string mensaje = personaService.Modificiar(tipoTasaInteres, persona);
+                Console.WriteLine(mensaje);
+            }
+
+        }
     }
 }
